a8fd825ca6c706545e5d560c548e0984b50c4463
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-12-25 22:06:22 -1000

    Update Taking-a-walk.js

--- Taking-a-walk.js
+++ Taking-a-walk.js
@@ -40,3 +40,10 @@ function isValidWalk(walk) {
 }
 
 // or
+
+function isValidWalk(walk) {
+  function count(val) {
+    return walk.filter(function(a){return a==val;}).length;
+  }
+  return walk.length==10 && count('n')==count('s') && count('w')==count('e');
+}
